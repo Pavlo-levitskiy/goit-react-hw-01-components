cefecd1970f48ad71b6442e50a5ead078ffa10f5
Pavlo-levitskiy <90378679+Pavlo-levitskiy@users.noreply.github.com>
2022-06-28 14:15:16 +0300

    react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import './styles/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+<<<<<<< ours
+=======
+  document.getElementById('root'),
+>>>>>>> theirs
+);
